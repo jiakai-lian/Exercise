3ace7c952b33ddae72b1888715db2b280bf80590
jiakailian <jiakai.lian@gmail.com>
2015-01-31 16:29:44 +1100

    add service API class

---
+++ DevCafe-iOS/Network/ServiceAPI.h
@@ -0,0 +1,15 @@
+//
+//  ServiceAPI.h
+//  a class for sending out service api request
+//  DevCafe
+//
+//  Created by jiakai lian on 31/01/2015.
+//  Copyright (c) 2015 jiakai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface ServiceAPI : NSObject
+
+@end
---
+++ DevCafe-iOS/Network/ServiceAPI.m
@@ -0,0 +1,15 @@
+//
+//  ServiceAPI.m
+//  a class for sending out service api request
+//  DevCafe
+//
+//  Created by jiakai lian on 31/01/2015.
+//  Copyright (c) 2015 jiakai. All rights reserved.
+//
+
+#import "ServiceAPI.h"
+#import <AFNetworking/AFNetworking.h>
+
+@implementation ServiceAPI
+
+@end
