f68bcb497b58001dec3d8cea42e1f2e7635e7ffb
jiakailian <jiakai.lian@gmail.com>
2015-01-31 14:47:49 +1100

    project setup

---
+++ DevCafe-iOS/Pods/Target Support Files/Pods-DevCafeTests-AFNetworking/Pods-DevCafeTests-AFNetworking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DevCafeTests_AFNetworking : NSObject
+@end
+@implementation PodsDummy_Pods_DevCafeTests_AFNetworking
+@end
---
+++ DevCafe-iOS/Pods/Target Support Files/Pods-DevCafeTests/Pods-DevCafeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DevCafeTests : NSObject
+@end
+@implementation PodsDummy_Pods_DevCafeTests
+@end
